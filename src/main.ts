0e6a384f8d3d9e51b175dc91bb6cfbb16e5bc867
Artem Ptashenko <aptashenko2019@gmail.com>
2025-08-29 00:40:43 +0200

    synchronize

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: "*",
-    },
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "http://localhost:5173", // локалка
+      "https://ai-carma.vercel.app", // твой фронт
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // если нужны куки/авторизация
   });
 
   app.useGlobalPipes(
