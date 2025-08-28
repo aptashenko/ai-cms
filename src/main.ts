9e2f77d3b34f119ba96413caf77e533d2753ea72
Artem Ptashenko <aptashenko2019@gmail.com>
2025-08-29 00:06:11 +0200

    synchronize

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import * as session from "express-session";
+import session from "express-session";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
