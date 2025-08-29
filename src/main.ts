59d785f0d56feadb6d5f5aade39ea6675a9f30fd
Artem Ptashenko <aptashenko2019@gmail.com>
2025-08-29 12:56:28 +0200

    synchronize

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "https://ai-carma.vercel.app", // фронтовый домен
-    credentials: true,
+    origin: [
+      "http://localhost:5173", // для разработки
+      "https://ai-carma.vercel.app", // для продакшена
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // какие нужны
+    credentials: false, // 👈 т.к. куки/токены НЕ используешь
   });
 
   app.useGlobalPipes(
