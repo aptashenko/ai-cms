f61a5ff0a1acdba35e34bd75873063d3c2d6c0a1
Artem Ptashenko <aptashenko2019@gmail.com>
2025-08-29 12:51:12 +0200

    synchronize

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*",
+    origin: "https://ai-carma.vercel.app", // фронтовый домен
     credentials: true,
   });
 
